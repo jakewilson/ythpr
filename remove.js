2343a74893189ce4432863e35a5d57f468de6a52
Jake Wilson <jakewilsonfl@gmail.com>
2017-04-02 19:43:09 -0400

    Shows content when the search button is clicked or enter is pressed.

--- remove.js
+++ remove.js
@@ -1,7 +1,27 @@
-let content = document.getElementById('content');
+function hideElement(element) {
+	element.style.display = 'none';
+}
+
+function showElement(element) {
+	content.style.display = '';
+}
+
 (function() {
-	content.style.display = 'none';
-	document.getElementById('search-btn').addEventListener('click', function() {
-		content.style.display = ''; // what it was initially
+	let content = document.getElementById('content');
+
+	hideElement(content);
+
+	// re-display the content when the user searches
+	document.getElementById('search-btn').addEventListener('click', function(event) {
+		showElement(content);
 	});
+
+	let ENTER_KEY = 13;
+
+	document.getElementById('masthead-search-term').addEventListener('keydown', function(event) {
+		if (event.keyCode === ENTER_KEY) {
+			showElement(content);
+		}
+	});
+
 })();
